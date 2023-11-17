a11f762b3a9b6de9530e0c546818ba9bdb3e8e14
izzetomarovcff <izzetomarovcff@gmail.com>
2023-11-17 16:26:14 +0400

    create category

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../categories/category.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
+  providers:[CategoryService]
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  saveCategory(name:any) {
+    this.categoryService.createCategory({ id: 0, name: name.value }).subscribe(data => {
+      this.router.navigate(["/home"]);
+    });
   }
 
 }
